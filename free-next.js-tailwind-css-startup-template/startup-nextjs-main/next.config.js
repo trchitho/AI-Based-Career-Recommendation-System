939dc4493306bdd6d3252bc07247e54cb2a4e13a
ptduong04 <tungduong22112004@gmail.com>
2025-10-06 19:38:00 +0700

    Login GG

---
+++ free-next.js-tailwind-css-startup-template/startup-nextjs-main/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["localhost", "lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
