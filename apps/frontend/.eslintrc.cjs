761b8053fc2300207c1b758f366fabd76702611a
Tho <tranchitho160704@gmail.com>
2026-06-21 07:34:32 +0700

    config(frontend): configure typescript eslint parser in .eslintrc.cjs

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -1,12 +1,24 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    plugins: ['@typescript-eslint', 'react-hooks'],
     extends: [
         'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     rules: {
-        'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+        '@typescript-eslint/no-explicit-any': 'off',
         'no-console': 'warn',
     },
 }
